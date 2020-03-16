1352268ce25e392d837541fb8dfd1d514a6a961a
ana9287 <abm.chamba@gmail.com>
2020-03-16 16:15:46 -0500

    Cálculadora básica-repaso angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'CALCULADORA';
+  titulo = 'CALCULADORA BÁSICA';
   operandoA: number;
   operandoB: number;
   resultadoSuma: number;
